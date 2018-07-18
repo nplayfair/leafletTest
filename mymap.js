2ba68c39a62d850f5ac445b64362847b3247ec6d
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-18 14:41:21 +0100

    bit of jquery shorthand

--- mymap.js
+++ mymap.js
@@ -1,25 +1,25 @@
-var myKey = config.MY_KEY;
-var mymap = L.map('mapid').setView([52.95, -1.15], 13);
+$(document).ready(function() {
+  var myKey = config.MY_KEY;
+  var mymap = L.map('mapid').setView([52.95, -1.15], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: myKey
-}).addTo(mymap);
-
-var popup = L.popup();
-
-//Add marker to map
-var marker = L.marker([52.9551,-1.1496]).addTo(mymap);
-
-function onMapClick(e) {
-    marker.setLatLng(e.latlng);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: myKey
+  }).addTo(mymap);
 
 
-    //Put long/lat into text box
-    var latlongFormatted = e.latlng.lat.toFixed(4) + "," + e.latlng.lng.toFixed(4);
-    document.querySelector("#selectedLonglat").value = latlongFormatted;
-}
+  //Add marker to map
+  var marker = L.marker([52.9551,-1.1496]).addTo(mymap);
 
-mymap.on('click', onMapClick);
+  function onMapClick(e) {
+      marker.setLatLng(e.latlng);
+
+      //Put long/lat into text box
+      var latlongFormatted = e.latlng.lat.toFixed(4) + "," + e.latlng.lng.toFixed(4);
+      document.querySelector("#selectedLonglat").value = latlongFormatted;
+  }
+
+  mymap.on('click', onMapClick);
+});
